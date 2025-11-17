8c38251eaf143c70c1a20742130770bf89d04627
Talha-glitched <m.talha2703@gmail.com>
2025-11-17 19:24:21 +0400

    Navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Link, useLocation } from "react-router";
-import websiteLogo from "@/Assets/Website-Logo.gif";
+import websiteLogo from "@/Assets/Website-Logo.png";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -27,10 +27,10 @@ export default function Navbar() {
     <motion.nav
       initial={{ y: -100 }}
       animate={{ y: 0 }}
-      className="fixed top-0 left-0 right-0 z-50"
+      className="fixed top-0 left-0 right-0 z-50 border-b border-black/40 bg-black/60 backdrop-blur-xl"
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-20">
+        <div className="flex justify-between items-center h-[90px]">
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
